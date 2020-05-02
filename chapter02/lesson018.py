0c293831e8bd193296a39657b0d033b64334a9ca
imasaki20 <imasaki20@gmail.com>
2020-05-02 19:50:09 +0900

    Modify Lesson 018.

--- chapter02/lesson018.py
+++ chapter02/lesson018.py
@@ -2,7 +2,7 @@ import sys
 
 def sort_by_num(path):
     with open(path) as f:
-        return "".join(sorted([line for line in f], key=lambda d: int(d.split("\t")[2])))
+        return "".join(sorted([line for line in f], reverse=True, key=lambda d: int(d.split("\t")[2])))
 
 if __name__ == "__main__":
     print(sort_by_num(sys.argv[1]))
